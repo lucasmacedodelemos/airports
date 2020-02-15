a63960f2a42b5d94a6651e3e8b090107741facc6
Lucas Macedo de Lemos <lucas.lemos2@br.experian.com>
2020-02-15 08:20:02 -0300

    Created airport project.

---
+++ Airports/AirportsTests/AirportsTests.swift
@@ -0,0 +1,34 @@
+//
+//  AirportsTests.swift
+//  AirportsTests
+//
+//  Created by Lucas Macedo de Lemos on 15/02/20.
+//  Copyright © 2020 Lucas Macedo de Lemos. All rights reserved.
+//
+
+import XCTest
+@testable import Airports
+
+class AirportsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
